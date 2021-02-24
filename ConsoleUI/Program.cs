0821faeccb43140d6dabbfdce0c59d9bba0a42da
osman <osman.anr@gmail.com>
2021-02-24 18:12:09 +0300

    24.02.2021

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleUI
         static void Main(string[] args)
         {
             CarManager carManager = new CarManager(new EfCarDal());
-            Car car1 = new Car() { Id=6, BrandId=1116, ColorId=2226, ModelYear=2021,Description="volvo", DailyPrice=200 };
+            Car car1 = new Car() { Id=6, BrandId=1116, ColorId=2226, ModelYear=2006,Description="Toyota", DailyPrice=150 };
             carManager.Add(car1);
 
             //CarManager carManager = new CarManager(new EfCarDal());
