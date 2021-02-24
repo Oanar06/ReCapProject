755eb91cfa5fbe693d2bc83fc9f5f89315929190
osman <osman.anr@gmail.com>
2021-02-24 17:43:21 +0300

    24.02.2021

--- Business/Abstract/ICarService.cs
+++ Business/Abstract/ICarService.cs
@@ -8,5 +8,10 @@ namespace Business.Abstract
     public interface ICarService
     {
         List<Car> GetAll();
+        List<Car> GetByModelear(decimal min, decimal max);
+        List<Car> GetCarsByBrandId(int BrandId);
+        List<Car> GetCarsByColorId(int ColorId);
+        void Add(Car car);
+        void Delete(Car car);
     }
 }
--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -14,9 +14,46 @@ namespace Business.Concrete
         {
             _carDal = carDal;
         }
+
+        public void Add(Car car)
+        {
+            if (car.Description.Length>=2 && car.DailyPrice > 0)
+
+            {
+                _carDal.Add(car);
+                Console.WriteLine("Araba eklendi:" + car.Description);
+            }
+            else
+            {
+                Console.WriteLine("Araba ismi minimum 2 karakter olmalı veAraba fiyatı 0'dan büyük olmalı");
+            }
+        }
+
+        public void Delete(Car car)
+        {
+            _carDal.Delete(car);
+        }
+
         public List<Car> GetAll()
         {
            return _carDal.GetAll();
+        }
+
+        public List<Car> GetByModelear(decimal min, decimal max)
+        {
+            return _carDal.GetAll(c => c.ModelYear >= min && c.ModelYear <= max);
+        }
+
+      
+
+        public List<Car> GetCarsByBrandId(int BrandId)
+        {
+            return _carDal.GetAll(c => c.BrandId == BrandId);
+        }
+
+        public List<Car> GetCarsByColorId(int ColorId)
+        {
+           return  _carDal.GetAll(c => c.ColorId == ColorId);
         }
     }
 }
